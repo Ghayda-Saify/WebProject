a12e53ab2fc23f2a6b1067a5a9de11fb23388654
Ghayda-Saify <g.safw2018@gmail.com>
2025-04-26 20:26:47 +0300

    home page - categories section

--- Home Page/app.js
+++ Home Page/app.js
@@ -77,3 +77,27 @@ showSlide(currentSlide);
 // Automatically change slide every 5 seconds
 setInterval(nextSlide, 5000);
 
+
+// categories slider
+var swiperCategory = new Swiper(".categories__container", {
+    spaceBetween: 24,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1400: {
+            slidesPerView: 6,
+            spaceBetween: 24,
+        },
+    },
+});
